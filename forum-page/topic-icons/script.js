191b16b9c6ddc75e1628464b7de0875c5287673d
CoryAR <83148797+CoryAR@users.noreply.github.com>
2025-06-12 14:15:37 -0500

    Create script.js

---
+++ forum-page/topic-icons/script.js
@@ -0,0 +1,31 @@
+// Originally by Quozzo of ZetaBoards
+// Converted by Cory of jCodes
+
+if ($('input[name="TopicTitle"]').length) {
+    $('#posting-form td.pformstrip:last').parent('tr').before('<tr><td class="pformleft">Topic Icon</td><td class="pformleft"><form id="t_icons"><label><input type="radio" name="tic" value="" checked="checked" />No Icon</label></form></td></tr>');
+
+    for (var i = 0; i < tIcons.length; i++) {
+        var index = tIcons.indexOf(tIcons[i]);
+        $('#t_icons').append('<label><input type="radio" name="tic" value="[ti]' + index + '[/ti]" /><img src="' + tIcons[i] + '" alt="Topic Icon" /></label>');
+    }
+
+    $('input[name="TopicTitle"]').val($('input[name="TopicTitle"]').val().replace(/\[ti\]([0-9]+)\[\/ti\]/gi, ''));
+
+    $('form[name="REPLIER"]').submit(function() {
+        var ticVal = $('#t_icons input:checked').val();
+        $('input[name="TopicTitle"]').val(ticVal + $('input[name="TopicTitle"]').val());
+    });
+}
+
+$('a:contains([/ti]), .maintitle:contains([/ti])').each(function() {
+    $(this).html($(this).html().replace(/\[ti\]([0-9]+)\[\/ti\]/gi, '<span class="t_icon">$1</span>'));
+    var num = $(this).find('span.t_icon').text();
+    $(this).find('span.t_icon').replaceWith('<img src="' + tIcons[num] + '" alt="Topic Icon" /> ');
+});
+
+var src = $('#posting-form .maintitle:has(img) img').attr('src');
+$('#t_icons img[src="' + src + '"]').prev('input').attr('checked', 'checked');
+
+if ($('title:contains([/ti])').length) {
+    $('title:contains([/ti])').html($('title:contains([/ti])').html().replace(/\[ti\]([0-9]+)\[\/ti\]/gi, ''));
+}
