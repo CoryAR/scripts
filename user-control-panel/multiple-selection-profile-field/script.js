112f2eac83060e7e50a2008b20a04067096b3cea
CoryAR <83148797+CoryAR@users.noreply.github.com>
2025-06-12 14:36:46 -0500

    Update script.js

--- user-control-panel/multiple-selection-profile-field/script.js
+++ user-control-panel/multiple-selection-profile-field/script.js
@@ -2,26 +2,30 @@
 // https://cory.jcink.net/
 
 function addExp(field, pID, href, src) {
-    if (location.href.indexOf('act=UserCP&CODE=01') !== -1) {
-        $('tr[id*="field_"] td:contains(' + field + ')').next('td').find('input').hide();
-        var input = $('tr[id*="field_"] td:contains(' + field + ')').next('td').find('input');
-        $('tr[id*="field_"] td:contains(' + field + ')').next('td').append('<span id="' + pID + '" style="cursor: pointer; opacity: 0.5; padding-left: 5px"><img src="' + src + '" alt="" title="' + pID + '" /></span>');
+   if (location.href.indexOf('act=UserCP&CODE=01') !== -1 || $('#registration-form').length) {
+       $('tr[id*="field_"] td:contains(' + field + '), fieldset legend:contains(' + field + ')').next('td, table').find('input').hide();
+       var input = $('tr[id*="field_"] td:contains(' + field + '), fieldset legend:contains(' + field + ')').next('td, table').find('input');
+       $('tr[id*="field_"] td:contains(' + field + ')').next('td').append('<span id="' + pID + '" style="cursor: pointer; opacity: 0.5; padding-left: 5px"><img src="' + src + '" alt="" title="' + pID + '"></span>');
+       $('fieldset legend:contains(' + field + ')').next('table').find('td:last').append('<span id="' + pID + '" style="cursor: pointer; opacity: 0.5; padding-left: 5px"><img src="' + src + '" alt="" title="' + pID + '"></span>');
 
-        if (input.val().match(pID)) {
-            $('span:has(img[title="' + pID + '"])').css('opacity', '1.0');
-        }
+       if (input.val().match(pID)) {
+           $('span:has(img[title="' + pID + '"])').css('opacity', '1.0');
+       }
 
-        $('#' + pID).click(function() {
-            if (input.val().match(pID)) {
-                $(this).css('opacity', '0.5');
-                input.val(input.val().split(' ' + pID)[0] + input.val().split(' ' + pID)[1]);
-            } else {
-                $(this).css('opacity', '1.0');
-                input.val(input.val() + ' ' + pID);
-            }
-        });
-    }
+       $('#' + pID).click(function() {
+           if (input.val().match(pID)) {
+               $(this).css('opacity', '0.5');
+               input.val(input.val().split(' ' + pID)[0] + input.val().split(' ' + pID)[1]);
+           } else {
+               $(this).css('opacity', '1.0');
+               input.val(input.val() + ' ' + pID);
+           }
+       });
+   }
 
-    if ($('td.profile-left div.row2:contains(' + field + ')').length || $('span.postdetails:contains(' + field + ')').length) {
-        $('td.profile-left div.row2:contains(' + field + '), span.postdetails:contains(' + field + ')').html($('td.profile-left div.row2:contains(' + field + '), span.postdetails:contains(' + field + ')').html().replace(pID, '<a href="' + href + '" target="_blank" class="dd_icon"><img src="' + src + '" alt="" title="' + pID + '" /></a>'));
-    }
+   if ($('div.multi-selection:contains(' + field + ')').length) {
+       $('div.multi-selection:contains(' + field + ')').each(function() {
+           $(this).html($(this).html().replace(pID, '<a href="' + href + '" target="_blank" class="dd_icon"><img src="' + src + '" alt="" title="' + pID + '"></a>'));
+       });
+   }
+}
