432f9c3b0b2eefc378a83e9b61afdeef20ba24a2
CoryAR <83148797+CoryAR@users.noreply.github.com>
2025-06-13 19:20:08 -0500

    Update script.js

--- board-index/online-user-avatars/script.js
+++ board-index/online-user-avatars/script.js
@@ -1,25 +1,32 @@
 // By Cory
 // https://cory.jcink.net/
 
-$.get('/index.php?act=Members&max_results=1000', function(data) {
-    data = data.replace(/src=/g, "data-src=");
-    let doc = new DOMParser().parseFromString(data, 'text/html');
-    $('span.username', doc).each(function() {
-        let username = $(this).text();
-        let avatar = $(this).next('span.avatar').text();
+export default function init({
+    onlineUserContainer,
+    avatarImgURL,
+    width,
+    height
+}) {
+    $.get('/index.php?act=Members&max_results=1000', function(data) {
+        data = data.replace(/src=/g, "data-src=");
+        let doc = new DOMParser().parseFromString(data, 'text/html');
+        $('span.username', doc).each(function() {
+            let username = $(this).text();
+            let avatar = $(this).next('span.avatar').text();
 
-        if (avatar === '') {
-            avatar = avatarImgURL;
-        }
-
-        $(onlineUserContainer + ' a').each(function() {
-            let username2 = $(this).text();
-
-            if (username === username2) {
-                $(this).html('<img src="' + avatar + '" title="' + username2 + '" alt="' + username2 + '" style="width: ' + width + 'px; height: ' + height + 'px">');
+            if (avatar === '') {
+                avatar = avatarImgURL;
             }
+
+            $(onlineUserContainer + ' a').each(function() {
+                let username2 = $(this).text();
+
+                if (username === username2) {
+                    $(this).html('<img src="' + avatar + '" title="' + username2 + '" alt="' + username2 + '" style="width: ' + width + 'px; height: ' + height + 'px">');
+                }
+            });
         });
     });
-});
 
-$(onlineUserContainer).html($(onlineUserContainer).html().replace(/,/g, ' '));
+    $(onlineUserContainer).html($(onlineUserContainer).html().replace(/,/g, ' '));
+}
