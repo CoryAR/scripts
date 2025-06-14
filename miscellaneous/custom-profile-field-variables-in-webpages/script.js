539255c779218ee799584f4078084799f56cc69a
CoryAR <83148797+CoryAR@users.noreply.github.com>
2025-06-13 19:38:48 -0500

    Update script.js

--- miscellaneous/custom-profile-field-variables-in-webpages/script.js
+++ miscellaneous/custom-profile-field-variables-in-webpages/script.js
@@ -1,23 +1,23 @@
 export default function init({
     inputAct
 }) {
-if (inputAct === 'Pages' && $('#innerwrapper').html().indexOf('field_') !== -1) {
-    $('#innerwrapper').html($('#innerwrapper').html().replace(/<!-- \|field_([0-9]+)\| -->/g, '<span class="field_$1"></span>'));
+    if (inputAct === 'Pages' && $('#innerwrapper').html().indexOf('field_') !== -1) {
+        $('#innerwrapper').html($('#innerwrapper').html().replace(/<!-- \|field_([0-9]+)\| -->/g, '<span class="field_$1"></span>'));
 
-    $.get('/index.php?act=UserCP&CODE=01', function(data) {
-        $('span[class*="field_"]').each(function() {
-            var $this = $(this);
-            var val;
-            $('#' + $(this).attr('class'), data).each(function() {
-                if ($(this).find('textarea, input').length) {
-                    val = $(this).find('textarea, input').val();
-                } else {
-                    val = $(this).find('select option:selected').text();
-                }
+        $.get('/index.php?act=UserCP&CODE=01', function(data) {
+            $('span[class*="field_"]').each(function() {
+                var $this = $(this);
+                var val;
+                $('#' + $(this).attr('class'), data).each(function() {
+                    if ($(this).find('textarea, input').length) {
+                        val = $(this).find('textarea, input').val();
+                    } else {
+                        val = $(this).find('select option:selected').text();
+                    }
 
-                $this.html(val);
-            });
-        });
-    });
-}
+                    $this.html(val);
+                });
+            });
+        });
+    }
 }
