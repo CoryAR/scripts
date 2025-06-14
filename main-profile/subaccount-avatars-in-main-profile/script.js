612de6cdcba02f27048ca6dca9b9d75543e4e5d6
CoryAR <83148797+CoryAR@users.noreply.github.com>
2025-06-13 19:12:42 -0500

    Update script.js

--- main-profile/subaccount-avatars-in-main-profile/script.js
+++ main-profile/subaccount-avatars-in-main-profile/script.js
@@ -1,20 +1,24 @@
 // By Cory
 // https://cory.jcink.net/
 
-var bName = location.href.split('.jcink.net')[0].split('https://')[1];
-$('form[name="subprofilemenu"]').hide();
+export default function init({
+    defaultAvatar
+}) {
+    var bName = location.href.split('.jcink.net')[0].split('https://')[1];
+    $('form[name="subprofilemenu"]').hide();
 
-$('form[name="subprofilemenu"] option').each(function() {
-    var uID = $(this).val();
-    var uName = $(this).text().replace('»', '');
+    $('form[name="subprofilemenu"] option').each(function() {
+        var uID = $(this).val();
+        var uName = $(this).text().replace('»', '');
 
-    if (uID !== '-------------------') {
-        $(this).parents('form[name="subprofilemenu"]').before('<div id="u-' + uID + '" class="sub_div" title="' + uName + '" style="background: url(https://files.jcink.net/uploads/' + bName + '//av-' + uID + '.png), url(https://files.jcink.net/uploads/' + bName + '//av-' + uID + '.gif), url(https://files.jcink.net/uploads/' + bName + '//av-' + uID + '.jpg), url(' + defaultAvatar + '); background-size: cover; width: 50px; height: 50px; display: inline-block; margin: 5px; cursor: pointer"></div>');
-    }
-});
+        if (uID !== '-------------------') {
+            $(this).parents('form[name="subprofilemenu"]').before('<div id="u-' + uID + '" class="sub_div" title="' + uName + '" style="background: url(https://files.jcink.net/uploads/' + bName + '//av-' + uID + '.png), url(https://files.jcink.net/uploads/' + bName + '//av-' + uID + '.gif), url(https://files.jcink.net/uploads/' + bName + '//av-' + uID + '.jpg), url(' + defaultAvatar + '); background-size: cover; width: 50px; height: 50px; display: inline-block; margin: 5px; cursor: pointer"></div>');
+        }
+    });
 
-$('div.sub_div').click(function() {
-    var uID = $(this).attr('id').split('u-')[1];
-    $('form[name="subprofilemenu"] select').val(uID);
-    $('form[name="subprofilemenu"]').submit();
-});
+    $('div.sub_div').click(function() {
+        var uID = $(this).attr('id').split('u-')[1];
+        $('form[name="subprofilemenu"] select').val(uID);
+        $('form[name="subprofilemenu"]').submit();
+    });
+}
