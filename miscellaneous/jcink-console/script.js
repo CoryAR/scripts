d6b8ba8213d6faf517197ad6540bb7bf7496b688
CoryAR <83148797+CoryAR@users.noreply.github.com>
2026-01-18 10:40:10 -0600

    Update script.js

--- miscellaneous/jcink-console/script.js
+++ miscellaneous/jcink-console/script.js
@@ -1,3 +1,6 @@
+// By Cory
+// https://cory.jcink.net/
+
 $('body').append(`<div id="jcink_console" style="position: fixed; z-index: 999; bottom: 10px; right: 10px; width: 67px"><div id="jcink_console-open" class="tableborder" style="cursor: pointer; border-radius: 10px; text-align: center"><div class="row2" style="border-radius: 10px">Open Jcink Console</div></div><div style="display: none" id="jcink_console-main" class="tableborder"><div class="maintitle" style="text-align: left">Jcink Console <span id="jcink_console-close" style="float: right; cursor: pointer">Close</span></div><div class="row2"><textarea cols="100" rows="100" style="width: 98%; height: 250px" id="jcink_console-textarea" class="textinput"></textarea><br><br><input type="button" id="jcink_console-button" class="forminput" value="Run Code" style="cursor: pointer"><select id="jcink_console-select" class="forminput" style="float: left"><option value="0">Select one:</option><option value="1">HTML</option><option value="2">CSS</option><option value="3">JavaScript</option></select><input type="button" id="jcink_console-clear" class="forminput" style="float: right; cursor: pointer" value="Clear"></div></div></div>`);
 
 $('#jcink_console-open').click(function() {
